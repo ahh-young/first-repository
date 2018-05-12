0c17107990c2624a2f4212d07c4c36564c5b1223
ahyoung Lee <ah0112200@gmail.com>
2018-05-12 12:21:23 +0900

    [edit]-[이름변경]

---
+++ practice.c
@@ -0,0 +1,20 @@
+#include<iostream>
+using namepsace std;
+class Rect
+{
+int width, height;
+public:
+Rect(width=0, height=0){
+this->width=width;
+this->height=height;
+}
+int getArea(){
+return width*height;
+}
+};
+
+int main()
+{
+Rect a(5,10);
+cout<<a.getArea()<<endl;
+}
